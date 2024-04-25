dcdd8ac1618a93dd151419ebc5150f9e25ef7563
Leticia Gimenez <leticiagimenez2ntb@gmail.com>
2024-04-25 11:54:45 -0300

    Primeira aula finalizada

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOCADORA DEV</title>
+    <link rel="stylesheet" href="./css/estilo.css">
+</head>
+<body>
+    <header>
+        <h1>LOCADORA DEV</h1>
+        <ul>
+            <li>
+                <a href="index.php?menu=home">Home</a>
+            </li>
+            <li>
+                <a href="index.php?menu=lista-vídeos">Vídeos</a>
+            </li>
+            <li>
+                <a href="index.php?menu=lista-categorias">Categorias</a>
+            </li>
+            <li>
+                <a href="index.php?menu=lista-clientes">Clientes</a>
+            </li>
+            <li>
+                <a href="index.php?menu=locacoes">Locações</a>
+            </li>
+        </ul>
+    </header>
+</body>
+</html>
